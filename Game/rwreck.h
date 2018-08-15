cd25b07a32db5caacfa2bf2b75f11248ed6eefc8
chanhlee <36870165+chanhlee@users.noreply.github.com>
2018-08-15 13:51:18 -0400

    Add files via upload

---
+++ Game/rwreck.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.7
+ * Invocation command was nin10kit --mode=3 --resize=12x12 rwreck ramblinwreck.png 
+ * Time-stamp: Saturday 07/14/2018, 18:16:34
+ * 
+ * Image Information
+ * -----------------
+ * ramblinwreck.png 12@12
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef RWRECK_H
+#define RWRECK_H
+
+extern const unsigned short ramblinwreck[144];
+#define RAMBLINWRECK_SIZE 288
+#define RAMBLINWRECK_LENGTH 144
+#define RAMBLINWRECK_WIDTH 12
+#define RAMBLINWRECK_HEIGHT 12
+
+#endif
+
